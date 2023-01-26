e865714d7cf602b51d817b41e2fe93071aebc3eb
Derek Ding <derek.ding727@gmail.com>
2023-01-26 23:24:08 +0800

    added more readme and simple features notes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # R_GIS_unit01
+This is the first chapter for my learning note for GIS-R.
+
+Unit 01 covers basic model and data types and some useful packages.
+It also contains some R and Rmarkdown review notes.
---
+++ r_review01.R
@@ -0,0 +1,27 @@
+# This is a short script for R basics, part 1
+# Pt1: tidyverse (dplyr, ggplot2...)
+# Load the data:
+
+# Check the titles
+
+
+# Pre-processing
+
+
+# Row/Column operations with tidyverse
+
+
+# Handling missing values:
+
+
+
+
+
+
+# Overview of cleaned data 
+
+
+
+# Q-GIS Connections
+
+
