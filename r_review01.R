6dceaceb69eccd2c7a1f9a177d064d424b6b312f
Derek Ding <derek.ding727@gmail.com>
2023-01-28 13:05:00 +0800

    added useful comments

--- r_review01.R
+++ r_review01.R
@@ -2,6 +2,8 @@
 # Pt1: tidyverse (dplyr, ggplot2...)
 
 # push: git push -u origin main
+# Reset connections: git config http.postBuffer 524288000
+# The above reset command can help when errno10054 pops up. 
 # Load the data:
 
 # Check the titles
