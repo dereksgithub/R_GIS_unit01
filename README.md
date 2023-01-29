c360eabe12f0e9c78cc60797ec9aef0ad2867a5a
Derek Ding <derek.ding727@gmail.com>
2023-01-29 14:35:52 +0800

    Modified comments

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# R_GIS_unit01
+# R GIS Learning Unit 01
 This is the first chapter for my learning note for GIS-R.
 
 Unit 01 covers basic model and data types and some useful packages.
 It also contains some R and Rmarkdown review notes.
+
+The project contains multiple rmd notebooks and r scripts for R-GIS learning 
+and reviewing.
 
 Section 1 will be R packages review.
 
--- r_review01.R
+++ r_review01.R
@@ -1,11 +1,42 @@
 # This is a short script for R basics, part 1
-# Pt1: tidyverse (dplyr, ggplot2...)
-
+################################ Notes for VCS: ############################
 # push: git push -u origin main
 # Reset connections: git config http.postBuffer 524288000
 # The above reset command can help when errno10054 pops up. 
-# Load the data:
+################################ Notes for VCS: ############################
 
+# Pt1: tidyverse (dplyr, ggplot2...)
+
+# load the packages needed:
+library(sf)
+library(tidyverse)
+library(terra)   
+library(spData)        # load geographic data
+library(spDataLarge)
+library(rgeos)
+library(rnaturalearth)
+# push: git push -u origin main
+# Reset connections: git config http.postBuffer 524288000
+# The above reset command can help when errno10054 pops up. 
+
+
+
+
+
+# Load the data:
+world_dfr = st_read(system.file("shapes/world.shp", package = "spData"))
+#> Reading layer `world' from data source 
+#>   `/usr/local/lib/R/site-library/spData/shapes/world.shp' using driver `ESRI Shapefile'
+#> Simple feature collection with 177 features and 10 fields
+#> Geometry type: MULTIPOLYGON
+#> Dimension:     XY
+#> Bounding box:  xmin: -180 ymin: -89.9 xmax: 180 ymax: 83.6
+#> Geodetic CRS:  WGS 84
+world_tbl = read_sf(system.file("shapes/world.shp", package = "spData"))
+class(world_dfr)
+#> [1] "sf"         "data.frame"
+class(world_tbl)
+#> [1] "sf"         "tbl_df"     "tbl"        "data.frame"
 # Check the titles
 
 
